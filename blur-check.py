06f9e405395175b7d297be7ab5f04781b381659b
maharshmellow <maharsh@live.ca>
2016-12-28 23:38:40 -0700

    added code

---
+++ blur-check.py
@@ -0,0 +1,12 @@
+import cv2
+
+imageName = "clouds.jpg"
+# read the image using opencv
+image = cv2.imread(imageName)
+# convert the image to grayscale
+grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# laplacian = second order derivative operator used to find edges
+# deemphasize regions with slowly varying gray levels -> produces edges in image
+# more blurry = less defined edges = lower variance
+variance = cv2.Laplacian(grayImage, cv2.CV_64F).var()
+print(variance)
